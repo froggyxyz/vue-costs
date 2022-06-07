18d6e9df0af4bbbba3c7e5ce34602681389180b8
Slava Ikonnikov <slava.ikonnikov5544@gmail.com>
2022-06-08 05:01:19 +0700

    work-1 completed

---
+++ src/components/costsButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <button @click="changeDisplay" class="btn">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  props: ['text'],
+  methods: {
+    changeDisplay() {
+      this.$emit('clicked');
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+// .btn {
+//   padding: 5px 15px;
+//   background: rgb(0, 185, 169);
+//   outline: 1px solid rgb(0, 52, 54);
+//   border: none;
+//   border-radius: 5px;
+//   cursor: pointer;
+// }
+//
+</style>
---
+++ src/components/paginationComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div v-if="costsNum > 5" class="pagination">
+    <button
+      v-for="num in Math.ceil(costsNum / 5)"
+      :key="num"
+      @click="changePage(num)"
+    >
+      {{ num }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['costsNum'],
+  methods: {
+    changePage(num) {
+      this.$emit('clickedPage', num);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.pagination {
+  max-width: 500px;
+  display: flex;
+  justify-content: space-between;
+  padding: 10px 15px;
+  outline: 1px solid rgb(196, 196, 196);
+  box-shadow: 1px 1px 5px rgb(115, 115, 115);
+  border-radius: 5px;
+}
+</style>
